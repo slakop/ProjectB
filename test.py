8034cf626ac7b623b47dd49d3523aeb23eef9c5d
v_kopychko <v_kopychko@inpas.ru>
2022-11-21 01:57:39 +0300

    test

---
+++ test.py
@@ -0,0 +1,18 @@
+import pytest
+
+def setup_module(module):
+    #init_something()
+    pass
+
+def teardown_module(module):
+    #teardown_something()
+    pass
+
+def test_upper():
+    assert 'foo'.upper() == 'FOO'
+    
+def test_isupper():
+    assert 'FOO'.isupper()
+    
+#def test_failed_upper():
+#    assert 'foo'.upper() == 'FOo'
